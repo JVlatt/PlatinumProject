b6dc84c28b340e7e729746374838dc4a297c39c6
Unknown <matthieu.manuellan@gmail.com>
2019-10-10 12:05:21 +0200

    ChunkGeneration

---
+++ Plat/Assets/Chunk.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+    [SerializeField]
+    private Transform m_startPosition;
+    public Transform startPosition
+    {
+        get { return m_startPosition; }
+        set { m_startPosition = value; }
+    }
+    [SerializeField]
+    private Transform m_endPosition;
+    public Transform endPosition
+    {
+        get { return m_endPosition; }
+        set { m_endPosition = value; }
+    }
+}
--- Plat/Assets/Scripts/LevelManager.cs
+++ Plat/Assets/Scripts/LevelManager.cs
@@ -5,13 +5,36 @@ using Assets.Script;
 
 public class LevelManager : MonoBehaviour
 {
+    [SerializeField]
+    private List<GameObject> m_chunkPrefabs = new List<GameObject>();
+    [SerializeField]
+    private List<Chunk> m_activeChunk = new List<Chunk>();
+    [SerializeField]
+    private float m_scrollSpeed = 1.0f;
+    [SerializeField]
+    private Transform m_resetPos;
+    [SerializeField]
+    private Transform m_spawnPos;
+
     private void Awake()
     {
         GameManager.GetManager()._levelManager = this;
     }
-
-    private void Start()
+    private void Update()
     {
-        Debug.Log(GameManager.GetManager()._soundManager.gameObject.name);
+        foreach(Chunk chunk in m_activeChunk)
+        {
+            chunk.transform.Translate(new Vector3(-m_scrollSpeed * Time.deltaTime,0,0));
+        }
+        if (m_activeChunk[0].endPosition.position.x <= m_resetPos.position.x)
+        {
+            Destroy(m_activeChunk[0].gameObject);
+            m_activeChunk.RemoveAt(0);
+        }
+        if(m_activeChunk[m_activeChunk.Count-1].endPosition.position.x <= m_spawnPos.position.x)
+        {
+            int random = Random.Range(0, m_chunkPrefabs.Count - 1);
+            m_activeChunk.Add(Instantiate(m_chunkPrefabs[random], m_activeChunk[m_activeChunk.Count - 1].endPosition.position, Quaternion.identity, transform).GetComponent<Chunk>());
+        }
     }
 }
