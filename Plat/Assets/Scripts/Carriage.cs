2330e4a1bb2026d198d48c370d6d6aa6b9458354
Unknown <matthieu.manuellan@gmail.com>
2019-10-16 15:23:59 +0200

    Déplacement fix

--- Plat/Assets/Scripts/Carriage.cs
+++ Plat/Assets/Scripts/Carriage.cs
@@ -8,8 +8,16 @@ public class Carriage : MonoBehaviour
     [SerializeField]
     private int m_capacity = 3;
 
+    [System.Serializable]
+    public class positions
+    {
+        public Transform position;
+        public bool isAvailable;
+        public Peon peonOnPos;
+    }
+
     [SerializeField]
-    private List<Transform> m_subDestinations = new List<Transform>(3);
+    private List<positions> m_subDestinations = new List<positions>();
 
     [SerializeField]
     private List<Peon> m_peons = new List<Peon>(3);
@@ -61,17 +69,26 @@ public class Carriage : MonoBehaviour
     {
         if(GameManager.GetManager()._peonManager._activePeon != null && m_peons.Count < m_capacity && !m_peons.Contains(GameManager.GetManager()._peonManager._activePeon))
         {
+
             Peon currentPeon = GameManager.GetManager()._peonManager._activePeon;
+            if (currentPeon._currentCarriage != null)
+            {
+                positions lastPos = currentPeon._currentCarriage.m_subDestinations.Find(x => x.peonOnPos == currentPeon);
+                lastPos.isAvailable = true;
+                lastPos.peonOnPos = null;
+                currentPeon._currentCarriage.RemovePeon(currentPeon);
+                
+            }
             currentPeon._destination = m_mainDestination.position;
             m_peons.Add(currentPeon);
             AddPeonToSpecialCarriage(currentPeon);
-            currentPeon._subDestination = m_subDestinations[m_peons.IndexOf(currentPeon)].position;
+            positions freepos = m_subDestinations.Find(x => x.isAvailable == true);
+            currentPeon._destination = m_mainDestination.position;
+            currentPeon._subDestination = freepos.position.position;
+            freepos.isAvailable = false;
+            freepos.peonOnPos = currentPeon;
             currentPeon._canMove = true;
-
-            if(currentPeon._currentCarriage !=null)
-            {
-                currentPeon._currentCarriage.RemovePeon(currentPeon);
-            }
+            
             
             currentPeon._currentCarriage = this;
 
