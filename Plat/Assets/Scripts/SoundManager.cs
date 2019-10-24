c7ad1e4cbbda5106a6742c8e1d91abc24f752454
Matt <matthieu.manuellan@gmail.com>
2019-10-24 22:19:49 +0200

    g push en premier

--- Plat/Assets/Scripts/SoundManager.cs
+++ Plat/Assets/Scripts/SoundManager.cs
@@ -6,7 +6,9 @@ using System;
 
 public class SoundManager : MonoBehaviour
 {
-    public Sound[] sounds;
+    public Sound[] fx;
+    public Sound[] ambient;
+    public Sound music;
     public static SoundManager instance;
 
     private void Awake()
@@ -19,7 +21,7 @@ public class SoundManager : MonoBehaviour
             return;
         }
         DontDestroyOnLoad(gameObject);
-        foreach (Sound s in sounds)
+        foreach (Sound s in fx)
         {
             s.source = gameObject.AddComponent<AudioSource>();
             s.source.clip = s.clip;
@@ -28,15 +30,35 @@ public class SoundManager : MonoBehaviour
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
         }
+        foreach (Sound s in ambient)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.outputAudioMixerGroup = s.mixer;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+            music.source = gameObject.AddComponent<AudioSource>();
+            music.source.clip = music.clip;
+            music.source.outputAudioMixerGroup = music.mixer;
+            music.source.volume = music.volume;
+            music.source.pitch = music.pitch;
+            music.source.loop = music.loop;
     }
     private void Start()
     {
         GameManager.GetManager()._soundManager = instance;
+        foreach(Sound s in ambient)
+        {
+            s.source.Play();
+        }
+        music.source.Play();
     }
 
     public void Play(string name)
     {
-        Sound s = Array.Find(sounds, sound => sound.name == name);
+        Sound s = Array.Find(fx, sound => sound.name == name);
         if (s == null)
         {
             Debug.Log("Sound" + name + "not found");
