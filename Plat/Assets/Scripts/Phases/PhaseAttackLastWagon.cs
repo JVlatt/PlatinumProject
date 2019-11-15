168dc3652f01f8dad0f64ff51f2d999ec5262f0d
Unknown <matthieu.manuellan@gmail.com>
2019-11-15 16:15:52 +0100

    nym

--- Plat/Assets/Scripts/Phases/PhaseAttackLastWagon.cs
+++ Plat/Assets/Scripts/Phases/PhaseAttackLastWagon.cs
@@ -24,14 +24,17 @@ public class PhaseAttackLastWagon : Phase
         {
             PhaseManager.Instance.NextPhase();
         }
-        Carriage c = TrainManager.Instance._carriages[TrainManager.Instance._carriages.Count - 1];
-        if (c != null)
+        else
         {
-            c.autoLoose = true;
-            c.Attack(duration, _timeBeforeAttack);
-            SoundManager.Instance.Play("attack");
-            PeonManager.Instance._activePeon = null;
-            _launchTimer = true;
+            Carriage c = TrainManager.Instance._carriages[TrainManager.Instance._carriages.Count - 1];
+            if (c != null)
+            {
+                c.autoLoose = true;
+                c.Attack(duration, _timeBeforeAttack);
+                SoundManager.Instance.Play("attack");
+                PeonManager.Instance._activePeon = null;
+                _launchTimer = true;
+            }
         }
     }
 
